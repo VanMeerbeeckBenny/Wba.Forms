7c41c5fc9f09932439a06bb7195d8b2d86547052
Benny <benny.van.meerbeeck@student.howest.be>
2021-11-23 17:10:53 +0100

    Changed classes bootstrap
    This is done so it matches the class from jquery validation

---
+++ Pre.Form-@-home.Web/Controllers/FormController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Pre.Form___home.Web.ViewModels;
+
+namespace Pre.Form___home.Web.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            SimpelFormViewModel simpelFormVm = new SimpelFormViewModel();
+            simpelFormVm.Subscribed = true;
+            return View(simpelFormVm);
+        }
+        [HttpPost]
+        public IActionResult Index(SimpelFormViewModel simpelFormVm)
+        {
+            if (ModelState.IsValid) return RedirectToAction("RecieveFormData", simpelFormVm);
+            else return View(simpelFormVm);
+        }
+       
+        public IActionResult RecieveFormData(SimpelFormViewModel simpelFormVm)
+        {
+            return View(simpelFormVm);            
+        }
+    }
+}
---
+++ Pre.Form-@-home.Web/ViewModels/SimpelFormViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pre.Form___home.Web.ViewModels
+{
+    public class SimpelFormViewModel
+    {
+        [Required(ErrorMessage = "Gelieve {0} in te vullen")]
+        [Display(Name = "Voornaam")]
+        [MaxLength(50,ErrorMessage ="{0} mag nier langer zijn dan {1}")]
+        [MinLength(2,ErrorMessage ="{0} moet groter zijn dan {1} charakters")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage ="Gelieve {0} in te vullen")]
+        [Display(Name = "Achternaam")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "Gelieve {0} in te vullen")]
+        [EmailAddress(ErrorMessage = "Gelieve een geldig {0} mee te geven")]
+        [Display(Name ="E-mail")]
+        public string Email { get; set; }
+        [Display(Name ="Subscribe me")]
+        public bool Subscribed { get; set; }
+    }
+}
