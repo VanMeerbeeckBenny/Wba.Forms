2dacb9289ee4c1843d4f07e07b8c87c83087c177
Benny <benny.van.meerbeeck@student.howest.be>
2021-11-24 16:09:27 +0100

    Add viewmodel for recieving data

--- Pre.Form-@-home.Web/Controllers/FormController.cs
+++ Pre.Form-@-home.Web/Controllers/FormController.cs
@@ -10,31 +10,44 @@ namespace Pre.Form___home.Web.Controllers
 {
     public class FormController : Controller
     {
-        public IActionResult Index()
+        SimpleSelectVm countrys = new SimpleSelectVm
         {
-            SimpelFormViewModel simpelFormVm = new SimpelFormViewModel();
-            simpelFormVm.SelectItem = new SimpleSelectVm
-            {
-                SelectCountryId = 1,
-                Countries = new List<SelectListItem>
+            SelectCountryId = 1,
+            Countries = new List<SelectListItem>
                 {
                     new SelectListItem{Value = "0", Text = "==Pick a Country=="},
                     new SelectListItem{Value = "1",Text ="Belgium"},
                     new SelectListItem{Value= "2",Text = "Nederland"},
                     new SelectListItem{Value = "3",Text="Duitsland"}
                 }
-            };
+        };
+        public IActionResult Index()
+        {
+            SimpelFormViewModel simpelFormVm = new SimpelFormViewModel();
+            simpelFormVm.SelectItem = countrys;
             simpelFormVm.Subscribed = true;
             return View(simpelFormVm);
         }
         [HttpPost]
         public IActionResult Index(SimpelFormViewModel simpelFormVm)
         {
-            if (ModelState.IsValid) return RedirectToAction("RecieveFormData", simpelFormVm);
+            if (ModelState.IsValid)
+            {
+                var RecievedAnswersvm = new SimpelFormRecievedAnswersViewModel 
+                { 
+                    FirstName = simpelFormVm.FirstName,
+                    LastName = simpelFormVm.LastName,
+                    Country = countrys.Countries[simpelFormVm.SelectItem.SelectCountryId].Text,
+                    Email = simpelFormVm.Email,
+                    Password = simpelFormVm.Password,
+                    Subscribed = simpelFormVm.Subscribed
+                };
+                return RedirectToAction("RecieveFormData", RecievedAnswersvm);
+            }             
             else return View(simpelFormVm);
         }
        
-        public IActionResult RecieveFormData(SimpelFormViewModel simpelFormVm)
+        public IActionResult RecieveFormData(SimpelFormRecievedAnswersViewModel simpelFormVm)
         {
             return View(simpelFormVm);            
         }
--- Pre.Form-@-home.Web/ViewModels/SimpelFormViewModel.cs
+++ Pre.Form-@-home.Web/ViewModels/SimpelFormViewModel.cs
@@ -13,16 +13,31 @@ namespace Pre.Form___home.Web.ViewModels
         [MaxLength(50,ErrorMessage ="{0} mag nier langer zijn dan {1}")]
         [MinLength(2,ErrorMessage ="{0} moet groter zijn dan {1} charakters")]
         public string FirstName { get; set; }
+
         [Required(ErrorMessage ="Gelieve {0} in te vullen")]
         [Display(Name = "Achternaam")]
         public string LastName { get; set; }
+
         [Required(ErrorMessage = "Gelieve {0} in te vullen")]
         [EmailAddress(ErrorMessage = "Gelieve een geldig {0} mee te geven")]
         [Display(Name ="E-mail")]
         public string Email { get; set; }
+
         [Display(Name ="Subscribe me")]
-        public bool Subscribed { get; set; }
-        
+        public bool Subscribed { get; set; }        
+
+        [Required]
         public SimpleSelectVm SelectItem { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name ="Paswoord")]
+        [Required(ErrorMessage = "Please fill in {0}")]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password),ErrorMessage ="Password do's not match!!")]
+        [DataType(DataType.Password)]
+        [Display(Name ="Herhaal paswoord")]
+        [Required(ErrorMessage = "Please fill in {0}")]
+        public string RepeatPassword { get; set; }
     }
 }
